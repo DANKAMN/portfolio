689cc5931d418efcf271568397a454255d089371
DANKAMN <danielnicholas1477@gmail.com>
2024-04-03 16:41:12 +0100

    Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,8 @@ export default function RootLayout({
       <body className={`${inter.className} bg-gray-50 text-gray-950`}>
         <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        
+        <Header />
         {children}
       </body>
     </html>
--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,11 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import estateImg from "@/public/daniel-estate.png";
+import imaginifyImg from "@/public/imaginify.png";
+import eatsImg from "@/public/daniel-eats.png";
+
 export const links = [
   {
     name: "Home",
@@ -25,24 +33,81 @@ export const links = [
   },
 ] as const;
 
+export const experiencesData = [
+  {
+    title: "Graduated bootcamp",
+    location: "Lagos, Nigeria",
+    description:
+      "I graduated from Aptech Computer Education with an ADSE (Advanced Diploma In Software Engineering) degree. In the following year I found a job as a front-end developer.",
+    icon: React.createElement(LuGraduationCap),
+    date: "DEC 2022",
+  },
+  {
+    title: "Freelance Front-End Developer",
+    location: "Remote",
+    description:
+      "I worked as a freelance front-end developer for 6 months with clients online.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2023",
+  },
+  {
+    title: "Full-Stack Open University",
+    location: "Helsinki, Finland",
+    description:
+      "I enrolled in the Full Stack Open University online. After that I upskilled to a full-stack developer. My stack includes React, Next.js, TypeScript, Tailwind, Mongoose and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "OCT 2023 - DEC 2023",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Real Estate",
+    description:
+      "A cutting-edge full-stack real estate marketplace. Users can post real estate listings and book listings",
+    tags: ["React", "Node.js", "MongoDB", "Google OAuth", "Firebase"],
+    imageUrl: estateImg,
+  },
+  {
+    title: "Imaginify",
+    description:
+      "Imaginfy is a cutting-edge Software-as-a-Service image editing application, enriched  with AI capabilities and a sophisticated payments and credits system.",
+    tags: ["Next.js", "TypeScript", "MongoDB", "Stripe", "Clerk"],
+    imageUrl: imaginifyImg,
+  },
+  {
+    title: "Daniel Eats",
+    description:
+      "Daniel Eats is an enterprise level food ordering platform. Enjoy effortless restaurant addition, menu exploration, order placement, and advanced search features. ",
+    tags: ["React", "TypeScript", "MongoDB", "Auth0", "Cloudinary"],
+    imageUrl: eatsImg,
+  },
+] as const;
+
 export const skillsData = [
   "HTML",
   "CSS",
+  "SASS / SCSS",
+  "Tailwind",
+  "Shadcn",
   "JavaScript",
   "TypeScript",
   "React",
+  "Redux Toolkit",
   "Next.js",
   "Node.js",
-  "Git",
-  "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
+  "Mongoose",
   "Express",
-  "PostgreSQL",
-  "Python",
-  "Django",
-  "Framer Motion",
+  "MongoDB",
+  "Axios",
+  "Git",
+  "GitHub",
+  "Prisma",
+  "Jest",
+  "GraphQL",
+  "NPM",
+  "Yarn",
+  "Vercel",
+  "Netlify",
+  "Render"
 ] as const;
